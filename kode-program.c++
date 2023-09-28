654a9c35b5a2524e62b300f6e8d4b82a81aaaeb3
Arvin Sultan Satria <arvinsultansatria@gmail.com>
2023-09-28 20:44:25 +0700

    first commit

---
+++ kode-program.c++
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+// Main program
+int main() {
+    // Deklarasi variabel dan tipe data
+    int angka = 10;
+    float pecahan = 3.14;
+    char karakter = 'A';
+
+    // Output
+    cout << "Variabel dan Tipe Data:" << endl;
+    cout << "Angka: " << angka << endl;
+    cout << "Pecahan: " << pecahan << endl;
+    cout << "Karakter: " << karakter << endl;
+
+    // Array satu dimensi
+    int arraySatuDimensi[5] = {1, 2, 3, 4, 5};
+
+    cout << "\nArray Satu Dimensi:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Elemen " << i << ": " << arraySatuDimensi[i] << endl;
+    }
+
+    // Array multidimensi dan for
+    int arrayMultiDimensi[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+    cout << "\nArray Multidimensi:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Elemen " << i << "," << j << ": " << arrayMultiDimensi[i][j] << endl;
+        }
+    }
+
+    // Penggunaan if dan for
+    int nilai = 85;
+
+    if (nilai >= 60) {
+        cout << "\nAnda lulus!" << endl;
+    } else {
+        cout << "\nAnda tidak lulus." << endl;
+    }
+
+    // Penggunaan while
+    int counter = 0;
+    while (counter < 5) {
+        cout << "\nNilai counter: " << counter << endl;
+        counter++;
+    }
+
+    // Penggunaan do while
+    int bilangan = 1;
+    do {
+        cout << "Bilangan: " << bilangan << endl;
+        bilangan++;
+    } while (bilangan <= 5);
+
+    // Input
+    int inputAngka;
+    cout << "\nMasukkan sebuah angka: ";
+    cin >> inputAngka;
+    cout << "Anda memasukkan: " << inputAngka << endl;
+
+    // Komentar
+    // Ini adalah komentar satu baris
+    /*
+    Ini adalah komentar
+    beberapa baris
+    */
+
+    return 0;
+}
---
+++ kode-program.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        // Deklarasi variabel dan tipe data
+        int angka = 10;
+        float pecahan = 3.14f;
+        char karakter = 'A';
+
+        // Output
+        System.out.println("Variabel dan Tipe Data:");
+        System.out.println("Angka: " + angka);
+        System.out.println("Pecahan: " + pecahan);
+        System.out.println("Karakter: " + karakter);
+
+        // Array satu dimensi
+        int[] arraySatuDimensi = {1, 2, 3, 4, 5};
+
+        System.out.println("\nArray Satu Dimensi:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Elemen " + i + ": " + arraySatuDimensi[i]);
+        }
+
+        // Array multidimensi dan for
+        int[][] arrayMultiDimensi = {{1, 2, 3}, {4, 5, 6}};
+
+        System.out.println("\nArray Multidimensi:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Elemen " + i + "," + j + ": " + arrayMultiDimensi[i][j]);
+            }
+        }
+
+        // Penggunaan if dan for
+        int nilai = 85;
+
+        if (nilai >= 60) {
+            System.out.println("\nAnda lulus!");
+        } else {
+            System.out.println("\nAnda tidak lulus.");
+        }
+
+        // Penggunaan while
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println("\nNilai counter: " + counter);
+            counter++;
+        }
+
+        // Penggunaan do while
+        int bilangan = 1;
+        do {
+            System.out.println("Bilangan: " + bilangan);
+            bilangan++;
+        } while (bilangan <= 5);
+
+        // Input
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int inputAngka;
+        System.out.print("\nMasukkan sebuah angka: ");
+        inputAngka = scanner.nextInt();
+        System.out.println("Anda memasukkan: " + inputAngka);
+
+        // Komentar
+        // Ini adalah komentar satu baris
+        /*
+        Ini adalah komentar
+        beberapa baris
+        */
+    }
+}
